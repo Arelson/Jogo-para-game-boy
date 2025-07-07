de65e652c253f6f0e0eff6056cb076421f7ffcfa
Lokassso <lucas.dominici@discente.ufma.br>
2025-07-07 02:44:40 -0300

    Jogo Finalizado
    Comparando com a ultima versão feita na Branch do Emanoel, foram adicionadas uma tela inicial, um menu inicial, transições, sons e também algumas otimizações que infelizmente retiraram a retro compatibilidade com o Game Boy não color (limitação do processador gráfico do mesmo não executa a inicialização de forma satisfatória). Mas é isso, ele funciona.

---
+++ Jogo Final/oJogo/assets/backgrounds/rede_rgb.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="rede_rgb" tilewidth="8" tileheight="8" tilecount="12" columns="6">
+ <image source="rede_rgb.png" width="48" height="16"/>
+</tileset>
